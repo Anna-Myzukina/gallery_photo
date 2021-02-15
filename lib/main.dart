7e1153c3bcb86b104d9e3a1369c014089ecc3ddd
Anna-Myzukina <anna.muzykina83@gmail.com>
2021-02-15 03:47:44 +0200

    Bug Fixed

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       title: 'Photo Gallery',
       theme: ThemeData(
-        primarySwatch: Colors.green,
+        primarySwatch: Colors.blue,
       ),
       home: PhotoGallery(title: 'Try your Photo Gallery'),
     );
